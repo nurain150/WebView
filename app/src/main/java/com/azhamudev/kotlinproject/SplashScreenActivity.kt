d1126a73e5d7723e29a6075d51f27b8fe9530baa
nurain150 <eyiteminurain@gmail.com>
2021-01-06 07:36:44 +0100

    update SplashScreenActivity.kt

--- app/src/main/java/com/azhamudev/kotlinproject/SplashScreenActivity.kt
+++ app/src/main/java/com/azhamudev/kotlinproject/SplashScreenActivity.kt
@@ -10,19 +10,56 @@ import com.google.android.gms.ads.MobileAds;
 
 class SplashScreenActivity : AppCompatActivity() {
 
+    private lateinit var mInterstitialAd: InterstitialAd
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
-    MobileAds.initialize(this) {}
-    
-    
+MobileAds.initialize(this,
+            "ca-app-pub-3940256099942544~3347511713")
 
-        Timer().schedule(3000) {
+        mInterstitialAd = InterstitialAd(this)
+        mInterstitialAd.adUnitId = "ca-app-pub-3940256099942544/1033173712"
+        mInterstitialAd.loadAd(AdRequest.Builder().build())
+
+mInterstitialAd.adListener = object: AdListener() {
+    override fun onAdLoaded() {
+        // Code to be executed when an ad finishes loading.
+    }
+
+    override fun onAdFailedToLoad(adError: LoadAdError) {
+        // Code to be executed when an ad request 
+Timer().schedule(3000) {
             val intent = Intent(applicationContext, WebViewActivity::class.java)
             startActivity(intent)
             finish()
         }
     }
+
+    override fun onAdOpened() {
+        // Code to be executed when the ad is displayed.
+    }
+
+    override fun onAdClicked() {
+        // Code to be executed when the user clicks on an ad.
+    }
+
+    override fun onAdLeftApplication() {
+        // Code to be executed when the user has left the app.
+    }
+
+    override fun onAdClosed() {
+        // Code to be executed when the interstitial ad is closed.
+Timer().schedule(3000) {
+            val intent = Intent(applicationContext, WebViewActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
+
+
+      
 
 
 }
