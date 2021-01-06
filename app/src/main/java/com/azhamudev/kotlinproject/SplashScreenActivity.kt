f581f894e294fe7c80a73d9930404ca354efcf6b
nurain150 <eyiteminurain@gmail.com>
2021-01-06 07:13:24 +0100

    update SplashScreenActivity.kt

--- app/src/main/java/com/azhamudev/kotlinproject/SplashScreenActivity.kt
+++ app/src/main/java/com/azhamudev/kotlinproject/SplashScreenActivity.kt
@@ -5,12 +5,17 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import java.util.*
 import kotlin.concurrent.schedule
+import com.google.android.gms.ads.MobileAds;
+
 
 class SplashScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+    MobileAds.initialize(this) {}
+    
+    
 
         Timer().schedule(3000) {
             val intent = Intent(applicationContext, WebViewActivity::class.java)
